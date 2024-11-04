fe5802fd2b30fa76e2fe6a3aa5c54d8f11f57104
DoriaFabio <doriafabio2001@gmail.com>
2024-11-04 19:54:20 +0100

    do snack3 and modify h1 and title

--- Snack3/snack3.js
+++ Snack3/snack3.js
@@ -1,2 +1,14 @@
 "use strict";
 console.clear();
+
+// Stampa le potenze di 2 fino a 1000.
+let risultato = 0;
+let i = 0;
+while (risultato < 1000 && i < 15) {
+    risultato = Math.pow(2, i);
+    if (risultato > 1000) {
+        break;
+    }
+    console.log("2 alla " + i + " è uguale a " + risultato);
+    i++;
+}
