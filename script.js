2b1031967dd8d5b65f3ad34c6e17e449c102ad04
DoriaFabio <doriafabio2001@gmail.com>
2024-11-04 17:56:38 +0100

    Add file html and file js

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+console.clear();
+// 1 Esercizio
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Non è necessario provvedere alla validazione delle email
+
+
+
+// 2 Esercizio
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
