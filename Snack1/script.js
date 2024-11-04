abf9a0784c25343238a796794b9032612fe34fa9
DoriaFabio <doriafabio2001@gmail.com>
2024-11-04 18:47:18 +0100

    Do snack 1 (array vuoto)

---
+++ Snack1/script.js
@@ -0,0 +1,18 @@
+"use strict";
+console.clear();
+
+// 1 Esercizio
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+let arrayV = [];
+for (let i = 1; i<=6; i++){
+    let x = parseInt(prompt("Inserisci un numero"));
+    console.log(x);
+    if(x % 2 != 0) {
+        arrayV.push(x);
+    }
+} 
+console.log(arrayV);
+
