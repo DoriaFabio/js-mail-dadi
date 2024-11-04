e5958dc0cfcadc2da42e133f630a36c644f59289
DoriaFabio <doriafabio2001@gmail.com>
2024-11-04 19:09:31 +0100

    do snack2 and modify H1 and title in file html

--- Snack2/snack2.js
+++ Snack2/snack2.js
@@ -3,3 +3,9 @@ console.clear();
 
 // Stampa il cubo dei primi N numeri,
 // dove N è un numero indicato dall’utente.
+
+const n = parseInt(prompt("Inserisci un numero")); 
+console.log(n);
+for (let i = 1; i<=n; i++) {
+    console.log("Il numero "+ i + " alla terza è " + Math.pow(i, 3));
+}
