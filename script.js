15d4a7d1f7cd42ee4e1b6e041e819feefedf5eff
DoriaFabio <doriafabio2001@gmail.com>
2024-11-04 18:14:00 +0100

    do first exercise

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ console.clear();
 // stampa un messaggio appropriato sull’esito del controllo.
 // Non è necessario provvedere alla validazione delle email
 
+const email = [
+    'nathan@gmail.com',
+    'alessandro@gmail.com',
+    'fabio@gmail.com',
+    'paolo@gmail.com',
+    'carlo@gmail.com',
+    'lewis@gmail.com',
+    'luca@gmail.com'
+];
+
+const emailutente = prompt("Inserisci la tua email");
+console.log(emailutente);
+let x = false;
+for(let i = 0; i < email.length; i++) {
+    if (emailutente==email[i]) {
+        console.log("Sei nella lista e puoi accedere");
+        x = true;
+    }
+} 
+if (x==false) {
+    console.log ("Non fai parte della lista e non puoi accedere");
+}
 
 
 // 2 Esercizio
